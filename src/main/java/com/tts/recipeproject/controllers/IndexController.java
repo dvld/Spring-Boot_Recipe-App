99ae369aba9634612f91bd817ca2016c5844b92a
dvld <nikoroberts620@gmail.com>
2019-06-11 14:42:53 -0500

    added Recipe Notes Ingredient and UnitOfMeasure classes

--- src/main/java/com/tts/recipeproject/controllers/IndexController.java
+++ src/main/java/com/tts/recipeproject/controllers/IndexController.java
@@ -1,5 +1,14 @@
 package com.tts.recipeproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
 
+//	@RequestMapping({"/", "", "/index"})
+	@GetMapping({"/", "", "/index"})
+	public String getIndexPage() {
+		return "index";
+	}
 }
